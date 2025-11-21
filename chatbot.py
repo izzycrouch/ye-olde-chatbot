49920c185821a68b3efc2171c69b5c142b70f538
izzycrouch <belle.crouch00@gmail.com>
2025-11-21 13:33:20 +0000

    Feat: Create generate_reply method on Chatbot class and test it works in main.py.

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,7 @@ class Chatbot:
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForCausalLM.from_pretrained(model_name)
-    
+
     def encode_prompt(self, prompt: str):
         encoded = self.tokenizer(prompt, return_tensors="pt")
         return encoded
@@ -15,4 +15,26 @@ class Chatbot:
     def decode_reply(self, reply_ids: list[int]):
         decoded = self.tokenizer.decode(reply_ids, skip_special_tokens=True)
         return decoded
+    
+    def generate_reply(self, prompt: str):
+        new_prompt = prompt + '\n'
+
+        encoded_prompt = self.encode_prompt(new_prompt)
         
+        prompt_ids = encoded_prompt['input_ids']
+        list_prompt_ids = prompt_ids[0].tolist()
+        prompt_len = len(list_prompt_ids)
+    
+        prompt_attention_mask = encoded_prompt['attention_mask']
+        
+        # generate = self.model.generate(input_ids=prompt_ids, attention_mask=prompt_attention_mask , pad_token_id=self.tokenizer.eos_token_id)
+        generate = self.model.generate(input_ids=prompt_ids, attention_mask=prompt_attention_mask , pad_token_id=self.tokenizer.eos_token_id, do_sample=True, top_p=0.8)
+
+        list_ids = generate[0].tolist()
+        
+        reply_ids = list_ids[prompt_len:]
+        
+        decoded_reply = self.decode_reply(reply_ids)
+        
+        return decoded_reply
+
--- main.py
+++ main.py
@@ -4,7 +4,17 @@
 from chatbot import Chatbot
 
 bot = Chatbot()
+
+# test encode_prompt method:
 # encoded = bot.encode_prompt("Hello, how are you?")
 # print(encoded)
-reply = bot.decode_reply([15496, 703, 345, 30])
-print(reply)
+
+# test decode_reply method:
+# reply = bot.decode_reply([15496, 703, 345, 30])
+# print(reply)
+
+
+prompt = "What is the weather like today?"
+reply = bot.generate_reply(prompt)
+print(f"Prompt: {prompt}")
+print(f"Reply: {reply}")
