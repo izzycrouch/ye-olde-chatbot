01ece09db6efb551bd42714f7f768237953b3348
izzycrouch <belle.crouch00@gmail.com>
2025-11-21 15:47:20 +0000

    Feat: Add new class property 'device' which checks if GPU is available to use.

--- chatbot.py
+++ chatbot.py
@@ -7,14 +7,24 @@ class Chatbot:
         model_name = "microsoft/DialoGPT-small"
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+        # self.model = AutoModelForCausalLM.from_pretrained(model_name)
 
         self.chat_history_ids = None
 
         self.system_prompt = "You are a helpful assistant. Respond to the end of this conversation accordingly.\n"
 
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = AutoModelForCausalLM.from_pretrained(model_name).to(self.device)
+        
+        if self.device == "cuda":
+            print("\nChatpot is using GPU acceleration.\n")
+    
+        else:
+            print("\nChatpot is not using GPU acceleration.\n")
+    
+
     def encode_prompt(self, prompt: str):
-        encoded = self.tokenizer(prompt, return_tensors="pt")
+        encoded = self.tokenizer(prompt, return_tensors="pt").to(self.device)
         return encoded
     
     def decode_reply(self, reply_ids: list[int]):
@@ -33,12 +43,12 @@ class Chatbot:
             encoded_system_prompt = self.encode_prompt(self.system_prompt)
             system_prompt_ids = encoded_system_prompt['input_ids']
             
-            input_ids = torch.cat((system_prompt_ids, prompt_ids), dim=1)
-            attention_mask = torch.ones_like(input_ids)
+            input_ids = torch.cat((system_prompt_ids, prompt_ids), dim=1).to(self.device)
+            attention_mask = torch.ones_like(input_ids).to(self.device)
         
         else:
-            input_ids = torch.cat((self.chat_history_ids, prompt_ids), dim=1)
-            attention_mask = torch.ones_like(input_ids)
+            input_ids = torch.cat((self.chat_history_ids, prompt_ids), dim=1).to(self.device)
+            attention_mask = torch.ones_like(input_ids).to(self.device)
 
         generate = self.model.generate(input_ids=input_ids, attention_mask=attention_mask , pad_token_id=self.tokenizer.eos_token_id, do_sample=True)
         
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from chatbot import Chatbot
 
-# bot = Chatbot()
+bot = Chatbot()
 
 # test encode_prompt method:
 # encoded = bot.encode_prompt("Hello, how are you?")
@@ -14,35 +14,35 @@ from chatbot import Chatbot
 # print(reply)
 
 # test generate_reply method:
-# prompts = [
-#     "What's your name?",
-#     "What do you think about AI?",
-#     "Sorry, tell me your name again."
-# ]
-# for prompt in prompts:
-#     reply = bot.generate_reply(prompt)
-#     print(f"Prompt: {prompt}")
-#     print(f"Reply: {reply}\n")
+prompts = [
+    "What's your name?",
+    "What do you think about AI?",
+    "Sorry, tell me your name again."
+]
+for prompt in prompts:
+    reply = bot.generate_reply(prompt)
+    print(f"Prompt: {prompt}")
+    print(f"Reply: {reply}\n")
 
-def use_chatbot():
-    bot = Chatbot()
-    print("\nBot: Welcome to Chatbot!\n")
+# def use_chatbot():
+#     bot = Chatbot()
+#     print("\nBot: Welcome to Chatbot!\n")
     
-    user = input("Enter your name: ")
-    question = input(f"\nBot: Hello {user}, how can Chatbot help? ")
+#     user = input("Enter your name: ")
+#     question = input(f"\nBot: Hello {user}, how can Chatbot help? ")
     
-    while True:
-        reply = bot.generate_reply(question)
-        print(f"\n{user}: {question}")
-        print(f"Bot: {reply}\n")
+#     while True:
+#         reply = bot.generate_reply(question)
+#         print(f"\n{user}: {question}")
+#         print(f"Bot: {reply}\n")
 
-        another_question = input(f"\nBot: Can I help you with anything else (y/n)?  ")
+#         another_question = input(f"\nBot: Can I help you with anything else (y/n)?  ")
     
-        if another_question != 'y':
-            print(f"Bot: Goodbye!\n")
-            bot.reset_history()
-            break
+#         if another_question != 'y':
+#             print(f"Bot: Goodbye!\n")
+#             bot.reset_history()
+#             break
             
-        question = input(f"\nBot: How can I help?  ")
+#         question = input(f"\nBot: How can I help?  ")
         
-use_chatbot()
+# use_chatbot()
