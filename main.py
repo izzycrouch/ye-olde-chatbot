59874fd74331216e7dd686d919e90a1e474e002b
izzycrouch <belle.crouch00@gmail.com>
2025-11-21 15:33:09 +0000

    Feat: create use_chatbot function in main.py.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from chatbot import Chatbot
 
-bot = Chatbot()
+# bot = Chatbot()
 
 # test encode_prompt method:
 # encoded = bot.encode_prompt("Hello, how are you?")
@@ -13,14 +13,36 @@ bot = Chatbot()
 # reply = bot.decode_reply([15496, 703, 345, 30])
 # print(reply)
 
+# test generate_reply method:
+# prompts = [
+#     "What's your name?",
+#     "What do you think about AI?",
+#     "Sorry, tell me your name again."
+# ]
+# for prompt in prompts:
+#     reply = bot.generate_reply(prompt)
+#     print(f"Prompt: {prompt}")
+#     print(f"Reply: {reply}\n")
 
-prompts = [
-    "What's your name?",
-    "What do you think about AI?",
-    "Sorry, tell me your name again."
-]
+def use_chatbot():
+    bot = Chatbot()
+    print("\nBot: Welcome to Chatbot!\n")
+    
+    user = input("Enter your name: ")
+    question = input(f"\nBot: Hello {user}, how can Chatbot help? ")
+    
+    while True:
+        reply = bot.generate_reply(question)
+        print(f"\n{user}: {question}")
+        print(f"Bot: {reply}\n")
 
-for prompt in prompts:
-    reply = bot.generate_reply(prompt)
-    print(f"Prompt: {prompt}")
-    print(f"Reply: {reply}\n")
+        another_question = input(f"\nBot: Can I help you with anything else (y/n)?  ")
+    
+        if another_question != 'y':
+            print(f"Bot: Goodbye!\n")
+            bot.reset_history()
+            break
+            
+        question = input(f"\nBot: How can I help?  ")
+        
+use_chatbot()
